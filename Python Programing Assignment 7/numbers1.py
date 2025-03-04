ee7a776f759ec5800e2bbca50025558e087d1482
Soumyodip Mukherjee_089 <soumyodip350@gmail.com>
2025-03-04 12:26:38 +0530

    Add files via upload

---
+++ Python Programing Assignment 7/numbers1.py
@@ -0,0 +1,8 @@
+# 2.Write a python program that creates a list of numbers from 1 to 20 that are divisible by 4
+
+divisible_by_4 = []
+for num in range(1, 21):
+    if num % 4 == 0:
+        divisible_by_4.append(num)
+
+print(divisible_by_4)
