68c95658fd969fed76d7e35d9922d8f188b8fd39
Soumyodip Mukherjee_089 <soumyodip350@gmail.com>
2025-03-04 12:10:30 +0530

    Add files via upload

---
+++ Python Programing Assignment 7/series.py
@@ -0,0 +1,19 @@
+# Write a python program to display the first ten terms of the following series 2 5 10 17
+
+def series(n):
+    terms = []
+    for i in range(1, n + 1):
+        term = i**2 + 1 
+        terms.append(term)
+    return terms
+
+def main():
+    n = int(input("Enter a range :")) 
+    terms = series(n)
+    print(f"First {n} Terms of the series:")
+    for i, term in enumerate(terms):
+        print(f"Term {i + 1} : {term}")
+
+if __name__ == "__main__":
+    main()
+    
